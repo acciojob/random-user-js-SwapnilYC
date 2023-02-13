db3f081dcb74df8e3e18c2fec79af4290c910a2d
SwapnilYC <119727525+SwapnilYC@users.noreply.github.com>
2023-02-13 20:36:38 +0530

    some code is added
    Code for event listeners for age button and all other 2 buttons is still pending. Basically event listeners inside of promise are not working

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your code here
+let img = document.getElementById('idPhotoDiv');
+let name = document.getElementById('name');
+let info = document.getElementById('displayInfo');
+let age = document.getElementById('idPhotoDiv');
+let email = document.getElementById('email');
+let phone = document.getElementById('phone');
+
+fetch(`https://randomuser.me/api/`)
+    .then((response) => response.json())
+    .then((product) => {
+        let obj = product.results[0];
+        console.log(obj)
+        let nameObj = obj.name;
+        let namee = `${nameObj.first} ${nameObj.last}`;
+        let agee = obj.dob.age;
+        let emaill = obj.email;
+        let phonee = obj.phone;
+        let imagee = obj.picture.large;
+        // console.log(imagee);
+
+        img.innerHTML = `<img src="${imagee}" alt="xyz">`;
+        name.innerText = `${namee}`;
+
+        age.addEventListener('click', ()=>{
+            info.innerText = `${agee}`;
+        })
+    })
+
