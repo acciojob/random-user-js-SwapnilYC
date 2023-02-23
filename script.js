7f6999149e46c0b852a2fcc121b0e3880db4760c
SwapnilYC <119727525+SwapnilYC@users.noreply.github.com>
2023-02-23 19:41:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,29 +1,75 @@
-//your code here
-let img = document.getElementById('idPhotoDiv');
-let name = document.getElementById('name');
-let info = document.getElementById('displayInfo');
-let age = document.getElementById('idPhotoDiv');
-let email = document.getElementById('email');
-let phone = document.getElementById('phone');
-
-fetch(`https://randomuser.me/api/`)
-    .then((response) => response.json())
-    .then((product) => {
-        let obj = product.results[0];
-        console.log(obj)
-        let nameObj = obj.name;
-        let namee = `${nameObj.first} ${nameObj.last}`;
-        let agee = obj.dob.age;
-        let emaill = obj.email;
-        let phonee = obj.phone;
-        let imagee = obj.picture.large;
-        // console.log(imagee);
-
-        img.innerHTML = `<img src="${imagee}" alt="xyz">`;
-        name.innerText = `${namee}`;
-
-        age.addEventListener('click', ()=>{
-            info.innerText = `${agee}`;
-        })
+let image = document.getElementById("image");
+let fullName = document.getElementById("fullName");
+let additionalInfo = document.getElementById("additionalInfo");
+let getAnoutherUser = document.getElementById("getUser");
+ 
+ 
+// getAnoutherUser.addEventListener("click", function(){
+//     fetch("https://randomuser.me/api/")
+//     .then((responce) => responce.json())
+//     .then((data)=>{
+//         let user = data.results[0];
+//         image.src = user.picture.large;
+//         fullName.innerText =  user.name.title + " " + user.name.first + " " + user.name.last;
+//         fullName.dataset.age = user.dob.age;
+//         fullName.dataset.email = user.email;
+//         fullName.dataset.phone = user.phone;
+//         additionalInfo.innerHTML = "";
+//     })
+// })
+ 
+// let ageButton = document.querySelector('[data-attr="age"]')
+// let emailButton = document.querySelector('[data-attr="email"]')
+// let phoneButton = document.querySelector('[data-attr="phone"]')
+ 
+// ageButton.addEventListener("click", function(){
+//     additionalInfo.innerHTML = fullName.dataset.age;
+// })
+ 
+// emailButton.addEventListener("click", function(){
+//     additionalInfo.innerHTML = fullName.dataset.email; 
+// })
+ 
+// phoneButton.addEventListener("click", function(){
+//     additionalInfo.innerHTML = fullName.dataset.phone; 
+// })
+ 
+// getAnoutherUser.click();
+ 
+ 
+ 
+let age;
+let email;
+let phone;
+ 
+getAnoutherUser.addEventListener("click", function(){
+    fetch("https://randomuser.me/api/")
+    .then((responce) => responce.json())
+    .then((data)=>{
+        let user = data.results[0];
+        image.src = user.picture.large;
+        fullName.innerText =  user.name.title + " " + user.name.first + " " + user.name.last;
+        age = user.dob.age;
+        email = user.email;
+        phone = user.phone;
+        additionalInfo.innerHTML = "";
     })
-
+})
+ 
+let ageButton = document.querySelector('[data-attr="age"]')
+let emailButton = document.querySelector('[data-attr="email"]')
+let phoneButton = document.querySelector('[data-attr="phone"]')
+ 
+ageButton.addEventListener("click", function(){
+    additionalInfo.innerHTML = age;
+})
+ 
+emailButton.addEventListener("click", function(){
+    additionalInfo.innerHTML = email; 
+})
+ 
+phoneButton.addEventListener("click", function(){
+    additionalInfo.innerHTML = phone; 
+})
+ 
+getAnoutherUser.click();
